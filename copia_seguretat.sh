789a21174c814c9a88bd3f9b1def2773d6daff84
IvanGP7 <ivangarciapallares@gmail.com>
2025-02-17 11:27:08 +0100

    Ready to deliver

--- copia_seguretat.sh
+++ copia_seguretat.sh
@@ -9,25 +9,25 @@ if [ $# == 0 ]; then
 fi
 
 # Check the Backup directory exist
-directory="/home/GSX/Backups/"
-mkdir -p "$directory"
+directory="/home/GSX/Backups/"                              # Save Path of the directory where we will save all backups
+mkdir -p "$directory"                                       # Create directory if does not exist
 
 # Get actual Date to create the name of the new file
 Date=$(date +"%Y-%m-%d_%Hh-%Mm-%Ss")
-buckup_file="$directory/backup_$Date.tgz"
+buckup_file="$directory/backup_$Date.tgz"                   # Name of the new Backup with the date
 
 # Show info recursively for all directories
 
 echo "*** Directories Information ***"
 for dir in $@; do 
-    if [ -d "$dir" ]; then
+    if [ -d "$dir" ]; then                                  # Show all atributes of the files we backup
         find $dir -exec stat --format=$'\nArchivo: %n\nTamaño: %s bytes\nPermisos: %A\nPropietario: %U\nGrupo: %G\nÚltimo acceso: %x\nÚltima modificación: %y\nÚltimo cambio: %z\n' {} \;
     else
         echo "Directory does NOT exist: $dir"
     fi
 done
 
-tar -p --atime-preserve -czf "$buckup_file" "$@"
+tar -p --atime-preserve -czf "$buckup_file" "$@"            # Compress all the files in the new backup
 
 if [ $? == 0 ]; then
     echo "SUCCESS: Creation Buckup file"
