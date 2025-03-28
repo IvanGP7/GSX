8ae101cde859be7afbeec3b652939455f7e5fc25
IvanGP7 <ivangarciapallares@gmail.com>
2025-03-28 10:17:44 +0100

    Entrega Final

---
+++ Setmana6/backup_files.sh
@@ -0,0 +1,4 @@
+sudo cp -r /var/lib/lxc/vm0/rootfs/root/dirrouter .
+sudo cp -r /var/lib/lxc/vm1/rootfs/root/dirdns .
+sudo cp -r /var/lib/lxc/vm2/rootfs/root/dirPC2 .
+sudo cp -r /var/lib/lxc/vm3/rootfs/root/dirwww .
--- Setmana6/copia_files.sh
+++ Setmana6/copia_files.sh
@@ -1,4 +1,4 @@
-sudo cp -r dirvm0 /var/lib/lxc/vm0/rootfs/root/
-sudo cp -r dirvm1 /var/lib/lxc/vm1/rootfs/root/
-sudo cp -r dirvm2 /var/lib/lxc/vm2/rootfs/root/
-sudo cp -r dirvm3 /var/lib/lxc/vm3/rootfs/root/
+sudo cp -r dirrouter /var/lib/lxc/vm0/rootfs/root/
+sudo cp -r dirdns /var/lib/lxc/vm1/rootfs/root/
+sudo cp -r dirPC2 /var/lib/lxc/vm2/rootfs/root/
+sudo cp -r dirwww /var/lib/lxc/vm3/rootfs/root/
