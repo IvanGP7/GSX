53d32c4def8f74b477dac9646b2cee8da89a29c2
IvanGP7 <ivangarciapallares@gmail.com>
2025-04-01 09:26:44 +0200

    Cambio nombre directorios para entrega

--- Setmana6/backup_files.sh
+++ Setmana6/backup_files.sh
@@ -1,4 +1,4 @@
-sudo cp -r /var/lib/lxc/vm0/rootfs/root/dirrouter .
-sudo cp -r /var/lib/lxc/vm1/rootfs/root/dirdns .
-sudo cp -r /var/lib/lxc/vm2/rootfs/root/dirPC2 .
-sudo cp -r /var/lib/lxc/vm3/rootfs/root/dirwww .
+sudo cp -r /var/lib/lxc/vm0/rootfs/root/dirvm0 .
+sudo cp -r /var/lib/lxc/vm1/rootfs/root/dirvm1 .
+sudo cp -r /var/lib/lxc/vm2/rootfs/root/dirvm2 .
+sudo cp -r /var/lib/lxc/vm3/rootfs/root/dirvm3 .
